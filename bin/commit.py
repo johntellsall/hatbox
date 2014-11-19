233cc7dfd0a854bb69ce1a90017d3a5f0eb8b4fd
John Mitchell <john@johntellsall.com>
2014-11-19 15:22:54 -0800

    add "--delete" demo; simplify main proc

--- bin/commit.py
+++ bin/commit.py
@@ -17,6 +17,11 @@ workflow:
 
 import argparse, re, subprocess, sys
 
+ISSUE_TYPES = {
+    'e': 'enhancements',
+    'b': 'bugs',
+}
+
 def get_cur_branch():
     return subprocess.check_output(
         'git rev-parse --abbrev-ref HEAD'.split()
@@ -35,28 +40,6 @@ def runproc(commands):
         print "command:", ' '.join(err.cmd)
         raise
         
-def cmd_push(args):
-    """
-    Push current branch upstream, renaming based on issue type.
-
-    commmit.py -p e
-    => git push origin HEAD:enhancements/123-my-title
-    """
-    
-    ISSUE_TYPES = {
-        'e': 'enhancements',
-        'b': 'bugs',
-    }
-    name = ISSUE_TYPES.get(args.label[0])
-    if not name:
-        sys.exit('Usage: commit -p (issue type); ie b=bugs, e=enhancements')
-        
-    if args.dry_run:
-        return
-    runproc(
-        ['git', 'push', 'origin', 'HEAD:{}/{}'.format(name, get_cur_branch())]
-    )
-
 def cmd_checkout(args):
     """
     Given an Issue title and ID, check out a new branch.
@@ -108,25 +91,47 @@ def cmd_commit(args):
         ['git', 'commit', '-am', message],
     )
 
+def cmd_delete(args):
+    name = ISSUE_TYPES.get(args.label[0])
+    if not name:
+        sys.exit('Usage: commit --delete (issue type); ie b=bugs, e=enhancements')
+        
+    print ' '.join(['git', 'push', 'origin', '--delete', '{}/{}'.format(name, get_cur_branch())])
+
+    
+def cmd_push(args):
+    """
+    Push current branch upstream, renaming based on issue type.
+
+    commmit.py -p e
+    => git push origin HEAD:enhancements/123-my-title
+    """
+    
+ 
+    name = ISSUE_TYPES.get(args.label[0])
+    if not name:
+        sys.exit('Usage: commit -p (issue type); ie b=bugs, e=enhancements')
+        
+    if args.dry_run:
+        return
+    runproc(
+        ['git', 'push', 'origin', 'HEAD:{}/{}'.format(name, get_cur_branch())]
+    )
 
 def main():
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('-c', '--checkout', dest='checkout', action='store_true')
+    parser.add_argument('-c', '--checkout', action='store_const', const=cmd_checkout, dest='func')
+    parser.add_argument('--delete', action='store_const', const=cmd_delete, dest='func')
     parser.add_argument('-n', dest='dry_run', action='store_true')
-    parser.add_argument('-p', '--push', dest='push', action='store_true')
+    parser.add_argument('-p', '--push', action='store_const', const=cmd_push, dest='func')
     parser.add_argument('label', type=str, nargs='+')
     args = parser.parse_args()
 
-    if args.checkout:
-        return cmd_checkout(args)
-    elif args.push:
-        return cmd_push(args)
-    cmd_commit(args)
+    if args.func:
+        return args.func(args)
+    else:
+        return cmd_commit(args)
     
-    sys.exit(0)
-    
-
-
 if __name__=='__main__':
     main()
 
